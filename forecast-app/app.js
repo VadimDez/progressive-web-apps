1a6ce352234822bc1f8ab3ca9473b4b83f21cb79
Vadym Yatsyuk <vasp3d@gmail.com>
2016-04-12 13:25:19 +0200

    service worker

--- forecast-app/app.js
+++ forecast-app/app.js
@@ -9,7 +9,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('service-worker.js')
       .then(function () {
         console.log('Service Worker registered');
       });
