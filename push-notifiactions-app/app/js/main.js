474663b3c42f7d8e4c5846458bb8ef8c552273c2
Vadym Yatsyuk <vasp3d@gmail.com>
2016-04-12 22:44:26 +0200

    subscribe/unsubscribe to push notifications

--- push-notifiactions-app/app/js/main.js
+++ push-notifiactions-app/app/js/main.js
@@ -1,18 +1,29 @@
 (function () {
   'use strict';
 
+  var reg;
+  var sub;
+  var $subscribeBtn = document.querySelector('.subscribe');
+  var isSubscribed = false;
+
   if ('serviceWorker' in navigator) {
 
     navigator.serviceWorker.register('sw.js')
-      .then(function (reg) {
+      .then(function () {
+        return navigator.serviceWorker.ready;
+      })
+      .then(function (serviceWorkerRegistration) {
+        reg = serviceWorkerRegistration;
+
+        $subscribeBtn.disabled = false;
 
         console.log(reg);
 
-        reg.pushManager.subscribe({
-          userVisibleOnly: true
-        }).then(function(sub) {
-          console.log('endpoint:', sub.endpoint);
-        });
+        //reg.pushManager.subscribe({
+        //  userVisibleOnly: true
+        //}).then(function(sub) {
+        //  console.log('endpoint:', sub.endpoint);
+        //});
       })
       .catch(function (err) {
         console.error(err);
@@ -34,4 +45,37 @@
     var data = {'registration_ids' : [ 'fVJwdWpt4Zg:APA91bHgR6ptek3yUHaqLHhcbl9bv5XpyoF44kOg8AR20ezNdf85y3bCq5C3e-650fHyzpg4tOPpokDONj-1t6vynP7ECi4xw5zaTVX6yIqwurYbECy_SC5njuWM9ATjdFHkMhFcQRKL' ], 'to': '/', 'data' : { 'message' : 'Push message sent via Android GCM.' } };
     request.send(JSON.stringify(data));
   });
+
+  $subscribeBtn.addEventListener('click', function () {
+    if (isSubscribed) {
+      unsubscribe();
+    } else {
+      subscribe();
+    }
+  });
+
+  function subscribe() {
+    reg.pushManager.subscribe({
+      userVisibleOnly: true
+    })
+      .then(function (pushSubscription) {
+        sub = pushSubscription;
+
+        console.log('subscribed. endpoint: ', sub.endpoint);
+        $subscribeBtn.textContent = 'Unsubscribe';
+        isSubscribed = true;
+      });
+  }
+
+  function unsubscribe() {
+    sub.unsubscribe().then(function () {
+      $subscribeBtn.textContent = 'Subscribe';
+      isSubscribed = false;
+
+      console.log('Unsubscribed.');
+    }).catch(function (error) {
+      console.log('Error during unsubscribe:', error);
+      $subscribeBtn.textContent = 'Subscribe';
+    })
+  }
 }());
