2c056c6f24879c8b5eeecc9116f8b52c2c92c679
Vadym Yatsyuk <vasp3d@gmail.com>
2016-04-13 00:02:10 +0200

    offline webapp

---
+++ air-horner/scripts/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by vadimdez on 12/04/16.
+ */
+(function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function () {
+        console.log('service worker registered');
+      });
+  }
+}());
