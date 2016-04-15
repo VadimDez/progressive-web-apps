9a509c1d6113995768c8cc2ccbd38738fd43a7d6
Vadym Yatsyuk <vasp3d@gmail.com>
2016-04-15 12:20:22 +0200

    fix path

--- air-horner/scripts/index.js
+++ air-horner/scripts/index.js
@@ -4,7 +4,7 @@
 (function () {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('sw.js')
       .then(function () {
         console.log('service worker registered');
       });
